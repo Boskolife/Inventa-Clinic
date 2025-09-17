5ad8e391d89efe81c93bd84ea335c0f00dd0ccc6
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-09-17 12:14:33 +0300

    add: main page

---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+window.addEventListener('load', () => {
+  setMenuTopPosition();
+  initBurger();
+});
+
+window.addEventListener('resize', () => {
+  setMenuTopPosition();
+  initBurger();
+});
+
+function initBurger() {
+  const burger = document.querySelector('.header_burger');
+  const menu = document.querySelector('.header_nav');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('open');
+    menu.classList.toggle('show');
+  });
+}
+
+function setMenuTopPosition() {
+  const header = document.querySelector('.header');
+  const menu = document.querySelector('.header_nav');
+
+  const headerHeight = header.offsetHeight;
+  if (window.innerWidth < 1200) {
+    menu.style.top = headerHeight + 'px';
+    menu.style.height = `calc(100dvh - ${headerHeight}px)`;
+  } else {
+    menu.style.removeProperty('top');
+    menu.style.removeProperty('height');
+  }
+}
+
+new Swiper('.doctors_swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 11,
+  modules: [Navigation],
+  speed: 1250,
+  navigation: {
+    nextEl: '.doctors-button-next.swiper-button-next',
+    prevEl: '.doctors-button-prev.swiper-button-prev',
+  },
+});
+
+new Swiper('.blog-section-swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 12,
+  modules: [Navigation],
+  speed: 1250,
+  navigation: {
+    nextEl: '.blog-section-button-next.swiper-button-next',
+    prevEl: '.blog-section-button-prev.swiper-button-prev',
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 import { htmlFiles } from './getHTMLFileNames';
+import viteImagemin from 'vite-plugin-imagemin';
 
 const input = { main: resolve(__dirname, 'src/index.html') };
 htmlFiles.forEach((file) => {
@@ -15,6 +16,11 @@ export default defineConfig({
   publicDir: '../public',
   plugins: [
     handlebars({ partialDirectory: resolve(__dirname, 'src/templates') }),
+    viteImagemin({
+      webp: {
+        quality: 80,
+      },
+    }),
   ],
   build: {
     rollupOptions: {
